c0955db814f6635ba9fe3758a1bb2bf4c5046565
Martin Brown <mbrown5151@googlemail.com>
2018-12-10 10:18:51 +0000

    Create rest api using Spring Intializr

---
+++ src/main/java/com/mb4321/springboot_restapi_v1_xp_simulations/SpringbootRestapiV1XpSimulationsApplication.java
@@ -0,0 +1,12 @@
+package com.mb4321.springboot_restapi_v1_xp_simulations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiV1XpSimulationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapiV1XpSimulationsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mb4321/springboot_restapi_v1_xp_simulations/SpringbootRestapiV1XpSimulationsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mb4321.springboot_restapi_v1_xp_simulations;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootRestapiV1XpSimulationsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
